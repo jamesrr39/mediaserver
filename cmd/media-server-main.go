ba08a000e746d7941ed0a52f69c2d00264c2975f
James <jr39.dev@gmail.com>
2018-02-27 10:47:22 +0100

    allow server to serve to other machines outside of localhost

--- cmd/media-server-main.go
+++ cmd/media-server-main.go
@@ -5,16 +5,17 @@ import (
 	"mediaserverapp/mediaserver"
 	"os"
 	"path/filepath"
-	"strconv"
 
 	"github.com/jamesrr39/goutil/userextra"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+const addrHelp = "Serves up via http on this address. Examples: 'localhost:9050' - serve on 9050 to localhost only. ':9050' serve to everyone on port 9050."
+
 var (
 	app              = kingpin.New("Mediaserver", "mediaserver")
-	imageRootDirFlag = app.Arg("Image Root Directory", "base directory to look for photos in").Required().String()
-	port             = app.Flag("port", "serves up via http on this port").Default("9050").Int()
+	imageRootDirFlag = app.Arg("Image Root Directory", "The base directory to look for photos in").Required().String()
+	addr             = app.Flag("addr", addrHelp).Default("localhost:9050").String()
 )
 
 func main() {
@@ -29,8 +30,8 @@ func main() {
 	if nil != err {
 		log.Fatalf("couldn't create a new media server and scan the pictures directory. Error: %s", err)
 	}
-	log.Printf("attempting to start serving on port %d\n", *port)
-	err = mediaServer.ServeHTTP("localhost:" + strconv.Itoa(*port))
+	log.Printf("attempting to start serving on address: '%s'\n", *addr)
+	err = mediaServer.ServeHTTP(*addr)
 	if nil != err {
 		log.Fatalln("Couldn't start HTTP server. Error: " + err.Error())
 	}
