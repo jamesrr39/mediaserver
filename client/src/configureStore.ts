2b67bdfa2e7297cfac6c5dab24c91558579d02b4
James <jr39.dev@gmail.com>
2018-05-20 22:37:29 +0200

    remove commented code

--- client/src/configureStore.ts
+++ client/src/configureStore.ts
@@ -1,25 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
-// import { Dispatch } from 'react-redux';
-// import { Action } from 'redux';
-// import { PICTURE_SUCCESSFULLY_UPLOADED } from './actions';
-
-// tslint:disable-next-line
-// const customMiddleware = (store: any) => (next: Dispatch<any>) => (action: Dispatch<any>) => {
-//   switch (action.type) {
-//     case PICTURE_SUCCESSFULLY_UPLOADED: {
-//       break;
-//     }
-//     default:{
-//       break;
-//     }
-//   }
-//
-//   // tslint:disable-next-line
-//   console.log("Middleware triggered:", action, store, next);
-//   next(action);
-// };
 
 // tslint:disable-next-line
 export default function configureStore(preloadedState?: any) {
@@ -28,7 +9,6 @@ export default function configureStore(preloadedState?: any) {
     preloadedState,
     applyMiddleware(
       thunkMiddleware,
-      // customMiddleware
     )
   );
 }
