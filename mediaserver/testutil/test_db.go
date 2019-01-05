3554e94dfac0a537e933aeaaae2c4a89c13d0f70
James <jr39.dev@gmail.com>
2019-01-05 19:28:20 +0100

    test db

--- mediaserver/testutil/test_db.go
+++ mediaserver/testutil/test_db.go
@@ -1,22 +1,32 @@
 package testutil
 
 import (
+	"bytes"
 	"fmt"
 	"mediaserverapp/mediaserver/dal/diskstorage/mediaserverdb"
 	"sync/atomic"
 	"testing"
 
 	"github.com/jamesrr39/goutil/logger"
-
 	"github.com/stretchr/testify/require"
 )
 
 var currentDbID int32
 
-func NewTestDB(t *testing.T) *mediaserverdb.DBConn {
+type TestDB struct {
+	*mediaserverdb.DBConn
+	Logger logger.Logger
+}
+
+func NewTestDB(t *testing.T) *TestDB {
 	dbID := atomic.AddInt32(&currentDbID, 1)
-	dbConn, err := mediaserverdb.NewDBConn(fmt.Sprintf("memory://testdb_%d", dbID), logger.Logger{})
+
+	logOutWriter := bytes.NewBuffer(nil)
+
+	dbLogger := logger.NewLogger(logOutWriter, logger.LogLevelInfo)
+
+	dbConn, err := mediaserverdb.NewDBConn(fmt.Sprintf("memory://testdb_%d", dbID), dbLogger)
 	require.Nil(t, err)
 
-	return dbConn
+	return &TestDB{dbConn, dbLogger}
 }
