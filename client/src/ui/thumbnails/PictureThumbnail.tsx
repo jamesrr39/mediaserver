fa47eceda49ecc53249a9ec35b8968ced7cf817f
James <jr39.dev@gmail.com>
2019-06-07 18:42:37 +0200

    fetch thumbnail with height 200

--- client/src/ui/thumbnails/PictureThumbnail.tsx
+++ client/src/ui/thumbnails/PictureThumbnail.tsx
@@ -3,6 +3,7 @@ import { PictureMetadata } from '../../domain/PictureMetadata';
 import { SERVER_BASE_URL } from '../../configs';
 import { joinUrlFragments } from '../../util/url';
 import { Size } from '../../domain/Size';
+import { THUMBNAIL_HEIGHTS } from '../../generated/thumbnail_sizes';
 
 type Props = {
     pictureMetadata: PictureMetadata,
@@ -19,18 +20,35 @@ export class PictureThumbnail extends React.Component<Props, ComponentState> {
     };
 
     render() {
-        if (!this.state.isLoaded) {
-            return null;
-        }
-
         const { size, pictureMetadata } = this.props;
 
-        const imgSrc = joinUrlFragments(SERVER_BASE_URL, 'picture', `${pictureMetadata.hashValue}?h=${size.height}`);
+        if (!this.state.isLoaded) {
+            const style = {
+                width: size.width + 'px',
+                height: size.height + 'px',
+                backgroundColor: '#ddd',
+            };
+            return <div style={style}></div>;
+        }
+
+        let thumbnailHeight = THUMBNAIL_HEIGHTS.find((height) => {
+            return (height >= size.height);
+        });
+        if (!thumbnailHeight) {
+            thumbnailHeight = pictureMetadata.rawSize.height;
+        }
+
+        const imgSrc = joinUrlFragments(
+            SERVER_BASE_URL,
+            'picture',
+            `${pictureMetadata.hashValue}?h=${thumbnailHeight}`
+        );
 
         const style = {
             width: size.width + 'px',
             height: size.height + 'px',
             backgroundImage: `url(${imgSrc})`,
+            backgroundSize: 'cover',
         };
 
         return <div style={style} />;
