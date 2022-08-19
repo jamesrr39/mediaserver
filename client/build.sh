df96ee1886dc02303c775f28a744a1979f9fbe3c
James <jr39.dev@gmail.com>
2022-08-19 19:24:19 +0200

    use concurrently to also watch CSS build

--- client/build.sh
+++ client/build.sh
@@ -6,32 +6,14 @@ set -e
 mkdir -p dist
 rm -rf dist/*
 
-# copy HTML index page, bootstrap CSS, and other static resources
-cp public/index.html dist/index.html
-# cp node_modules/bootstrap/dist/css/bootstrap.min.css dist/
-# cp -r node_modules/bootstrap-icons dist/
+# copy HTML index page and other static resources
+cp public/* dist/
 
-echo 'Start bundling CSS'
+echo 'Start bundling'
 
-# esbuild the CSS bundle
-esbuild src/index.css \
-    --loader:.png=dataurl \
-    --loader:.woff2=dataurl \
-    --loader:.woff=dataurl \
-    --loader:.ttf=dataurl \
-    --loader:.eot=dataurl \
-    --loader:.svg=dataurl \
-    --bundle \
-    --outfile=dist/main.css
+CSS_LOADERS="--loader:.png=dataurl --loader:.woff2=dataurl --loader:.woff=dataurl --loader:.ttf=dataurl --loader:.eot=dataurl --loader:.svg=dataurl"
+CSS_BUNDLE_CMD="esbuild src/index.css ${CSS_LOADERS} --watch --bundle --outfile=dist/main.css"
 
-echo 'Finished bundling CSS. Please note, CSS is not "watched" and to re-bundle, you have to run '
-echo 'Start bundling js'
+JS_BUNDLE_CMD="esbuild src/index.tsx --loader:.png=dataurl --watch --bundle --outfile=dist/main.js"
 
-# esbuild the JS bundle
-esbuild src/index.tsx \
-    --loader:.png=dataurl \
-    --watch \
-    --bundle \
-    --outfile=dist/main.js
-
-echo 'Finished bundling js'
+concurrently "${CSS_BUNDLE_CMD}" "${JS_BUNDLE_CMD}"
