6fef441b6566570f77626512c005804171fbd0c1
James <jr39.dev@gmail.com>
2022-08-22 11:19:17 +0200

    js source map

--- client/build.sh
+++ client/build.sh
@@ -14,6 +14,6 @@ echo 'Start bundling'
 CSS_LOADERS="--loader:.png=dataurl --loader:.woff2=dataurl --loader:.woff=dataurl --loader:.ttf=dataurl --loader:.eot=dataurl --loader:.svg=dataurl"
 CSS_BUNDLE_CMD="esbuild src/index.css ${CSS_LOADERS} --watch --bundle --outfile=dist/main.css"
 
-JS_BUNDLE_CMD="esbuild src/index.tsx --loader:.png=dataurl --watch --bundle --outfile=dist/main.js"
+JS_BUNDLE_CMD="esbuild src/index.tsx --loader:.png=dataurl --watch --bundle --sourcemap --outfile=dist/main.js"
 
 concurrently "${CSS_BUNDLE_CMD}" "${JS_BUNDLE_CMD}"
